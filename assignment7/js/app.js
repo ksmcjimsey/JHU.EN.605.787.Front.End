28fdd9733205fcf66b147d007fef07c5cc4125c1
Kevin Mc Jimsey <ksmcjimsey@users.noreply.github.com>
2020-03-10 13:01:56 -0700

    List movement is working

--- assignment7/js/app.js
+++ assignment7/js/app.js
@@ -10,7 +10,7 @@
     .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 
-    // Controller for the to buy list.
+    // Controller 1 for the to buy list.
     ToBuyController.$inject = ['ShoppingListCheckOffService'];
     function ToBuyController(ShoppingListCheckOffService) {
 
@@ -20,19 +20,29 @@
 
         // This bound items on html gets the updated list from the service.
         toBuy.items = ShoppingListCheckOffService.getToBuyList();
-        console.log(toBuy.items)
+        //console.log(toBuy.items)
+
+        toBuy.boughtItem = function (itemIdx) {
+        
+            // *****Need to call the service funtion to transfer the item
+            // this means remove from one list and add to the other.
+            ShoppingListCheckOffService.moveItemToBoughtList(itemIdx);
+
+        }
 
 
     }
 
 
-    // Controller for Already Bought list.
+    // Controller 2 for Already Bought list.
     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
     function AlreadyBoughtController(ShoppingListCheckOffService) {
 
         // Instead of exposing values through $scope we use the "this"
         // of the controller.
         var alreadyBought = this;
+
+        alreadyBought.items = ShoppingListCheckOffService.alreadyBoughtList();
 
     }
 
@@ -59,6 +69,29 @@
             return toBuyListArray;
         }
 
+        // Function to return the Already Bought list
+        service.alreadyBoughtList = function () {
+            return boughtListArray;
+        }
+
+        // Function to remove item from the toBuyListArray and 
+        // add it to the boughtList Array
+        service.moveItemToBoughtList = function (itemIndex) {
+
+            // If the index is in the array range then remove it
+            if (itemIndex > -1 && itemIndex < toBuyListArray.length) {
+                console.log("made it ", itemIndex);
+                
+                // Add item to bought list
+                boughtListArray.push(toBuyListArray[itemIndex]);
+
+
+                // Remove item from buy list
+                toBuyListArray.splice(itemIndex, 1);
+
+            }
+        }
+
     }
 
 
@@ -68,4 +101,6 @@
 // Steps
 // 1. Set up data in service
 // 2. Set up method to manage data in service
-// 3. Controller 
+// 3. Set up controllers to use the data they need for their section
+// 4. Set up the page sections to use the controllers and call the 
+//    controller methods to edit the page

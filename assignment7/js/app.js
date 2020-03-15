4a9b664f164c8c52f7d5c66fff3c9ea64ac778e4
Kevin Mc Jimsey <ksmcjimsey@users.noreply.github.com>
2020-03-15 13:31:01 -0700

    2a added price per item to the array elements

--- assignment7/js/app.js
+++ assignment7/js/app.js
@@ -55,14 +55,15 @@
 
         // Variables
         var toBuyListArray = [
-            { name : "cookies", quantity: 10 },
-            { name : "steak", quantity: 10 },
-            { name : "bag of potatoes", quantity: 1 },
-            { name : "corn on cob", quantity: 10 },
-            { name : "sour cream tub", quantity: 1 }
+            { name : "cookies", quantity: 10, pricePerUnit: 3.29 },
+            { name : "steak", quantity: 10, pricePerUnit: 21.99 },
+            { name : "bag of potatoes", quantity: 1, pricePerUnit: 3.99 },
+            { name : "corn on cob", quantity: 10, pricePerUnit: 0.25 },
+            { name : "sour cream tub", quantity: 1, pricePerUnit: 2.49 }
         ];
 
         var boughtListArray = [];
+
 
         // Function to return To Buy list
         service.getToBuyList = function () {
@@ -73,6 +74,7 @@
         service.alreadyBoughtList = function () {
             return boughtListArray;
         }
+
 
         // Function to remove item from the toBuyListArray and 
         // add it to the boughtList Array
@@ -92,6 +94,8 @@
             }
         }
 
+        
+
     }
 
 
@@ -104,3 +108,7 @@
 // 3. Set up controllers to use the data they need for their section
 // 4. Set up the page sections to use the controllers and call the 
 //    controller methods to edit the page
+
+// Minified test
+//
+// !function(){"use strict";function t(t){this.items=t.getToBuyList(),this.boughtItem=function(e){t.moveItemToBoughtList(e)}}function e(t){this.items=t.alreadyBoughtList()}angular.module("ShoppingListApp",[]).controller("ToBuyController",t).controller("AlreadyBoughtController",e).service("ShoppingListCheckOffService",function(){var t=[{name:"cookies",quantity:10},{name:"steak",quantity:10},{name:"bag of potatoes",quantity:1},{name:"corn on cob",quantity:10},{name:"sour cream tub",quantity:1}],e=[];this.getToBuyList=function(){return t},this.alreadyBoughtList=function(){return e},this.moveItemToBoughtList=function(o){o>-1&&o<t.length&&(console.log("made it ",o),e.push(t[o]),t.splice(o,1))}}),t.$inject=["ShoppingListCheckOffService"],e.$inject=["ShoppingListCheckOffService"]}();

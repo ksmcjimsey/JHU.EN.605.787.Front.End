08f9d0facd56740e372aed4f4add900b16218272
Kevin Mc Jimsey <ksmcjimsey@users.noreply.github.com>
2020-05-03 23:25:54 -0700

    Messaging on the form is working

--- assignment10/src/common/menu.service.js
+++ assignment10/src/common/menu.service.js
@@ -5,27 +5,54 @@ angular.module('common')
 .service('MenuService', MenuService);
 
 
+
 MenuService.$inject = ['$http', 'ApiPath'];
 function MenuService($http, ApiPath) {
   var service = this;
 
+  // SERVICE DATA
+  var userData = {};
+
+
+  // SERVICE METHODS
+  // Get the list of categories and return the list of category objects
   service.getCategories = function () {
-    return $http.get(ApiPath + '/categories.json').then(function (response) {
-      return response.data;
+    return $http.get(ApiPath + '/categories.json')
+    .then(function (response) {
+        return response.data;
     });
   };
 
 
+  // Get the list of items when passed the category short name
   service.getMenuItems = function (category) {
     var config = {};
     if (category) {
       config.params = {'category': category};
     }
 
-    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+    return $http.get(ApiPath + '/menu_items.json', config)
+    .then(function (response) {
       return response.data;
     });
   };
+
+
+  // Check if the short name is valid
+  // This must return a promise instead of a value due to the 
+  service.getShortNamePromise = function (shortName) {
+      return $http.get(ApiPath + '/menu_items/' + shortName + ".json")
+      
+  }
+
+
+  // Save the user data to the controller
+  service.setUserPreferences = function(userPref) {
+    
+  }
+
+  // Retrieve a short name data from the server and return the data
+
 
 }
 
--- assignment10/src/public/sign-up/sign-up.controller.js
+++ assignment10/src/public/sign-up/sign-up.controller.js
@@ -5,31 +5,54 @@
     .controller('SignUpController', SignUpController);
     
     // Need to inject the services
-    SignUpController.$inject = [];
-    function SignUpController() {
+    SignUpController.$inject = ['MenuService'];
+    function SignUpController(MenuService) {
 
         var $ctrl = this;
         console.log("Made it to MenuController");
 
         // Error message control
-        $ctrl.isValid = true;
+        $ctrl.isSNValid = true;
+        //console.log("Is short name valid?", $ctrl.isSNValid)
 
         // Success message control
         $ctrl.successfulSubmit = false;
       
-        // Submit function
+
+        // Submit method
         $ctrl.submit = function () {
+
+        // Check values from the form
         console.log("Name", $ctrl.user.firstname + " " + $ctrl.user.lastname);
+        console.log("Email", $ctrl.user.email);
+        console.log("Phone Number", $ctrl.user.phone);
+        console.log("Short Name", $ctrl.user.email);
 
         // Call service to see if the short name is valid
         // Can check error code or maybe other fields if I can't figure
         // out error code.
+        
+        /*** need the promise returned here and then check if it is true or not
+             in the then and catch.  Set values inside the then and catch    */
+        $ctrl.shortNamePromise = MenuService.getShortNamePromise($ctrl.user.shortName);
 
-        // If not valid set error message to true
+        $ctrl.shortNamePromise.then(function (respone) {
+            //console.log("Got items in isShortNameValid");
 
-        // If valid set completed message to true
+            // Call service to save the data
 
-        // Call service to save the data
+            // If valid, set completed message to true
+            $ctrl.successfulSubmit = true;
+          })
+          .catch(function (error) {
+            //console.log(error.data);
+            // If not valid, set error message to true
+            $ctrl.isSNValid = false;
+        });
+        
+        // All needs to go into the promise that is returned.
+        // If not valid, set error message to true
+
   }
 
     }

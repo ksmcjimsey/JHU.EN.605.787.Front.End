4c358cb7a13ab8969684876ef4b412734d6cbc55
Kevin Mc Jimsey <ksmcjimsey@users.noreply.github.com>
2020-05-05 20:19:45 -0700

    Done with Task 2

--- assignment10/src/public/my-info/user-data.controller.js
+++ assignment10/src/public/my-info/user-data.controller.js
@@ -4,24 +4,32 @@
 angular.module('public')
 .controller('UserDataController', UserDataController);
 
-UserDataController.$inject = ['userPref'];
-function UserDataController(userPref) {
+UserDataController.$inject = ['userPref', 'ApiPath'];
+function UserDataController(userPref, ApiPath) {
 
   var $ctrl = this;
-
   $ctrl.hasValue =  false;
-  $ctrl.phoneHasValue = true;
-
-
-  console.log("in UserDataController looking at userPref service return",
-  userPref);
-
+  $ctrl.phoneHasValue = false;
+  $ctrl.basePath = ApiPath;
+  
+  // Did the user set the user values yet?
   if (Object.keys(userPref).length==0) {
     console.log ("Object is empty");
+    $ctrl.hasValue =  false;
+    $ctrl.phoneHasValue = false;
   }
 
+  // User has set up preferences so check if phone number is set.
+  else {
+    console.log ("Object has keys and thus values");
+    $ctrl.hasValue =  true;
 
-
+    if (userPref.userData.phone) {
+      $ctrl.phoneHasValue = true;
+    } else {
+      $ctrl.phoneHasValue = false;
+    }
+  }
   
   $ctrl.userPref = userPref;
   console.log("Assign $ctrl.userPref: ", $ctrl.userPref);
